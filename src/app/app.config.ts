4a90631d6b1ce8c825718f0d07f73f7b1a884ee9
lana rodion <lana.rodion@gmail.com>
2025-04-07 12:57:19 +0200

    Add redirectTo and Remove withDebugTracing()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,7 +15,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideProtractorTestingSupport(),
-    provideRouter(routeConfig, withDebugTracing(), withComponentInputBinding()),
+    provideRouter(routeConfig, withComponentInputBinding()),
     provideClientHydration(withEventReplay()),
   ],
 };
+
+// withDebugTracing(): This is a debugging tool that can be used to trace
+// the execution of the router. It can be useful for debugging complex
+// routing scenarios, but it can also add overhead to the application.
+// It is not recommended to use this in production code.
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
 
 export const routeConfig: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, title: 'Home page' },
   { path: 'details/:id', component: DetailsComponent, title: 'Details page' },
   { path: 'get-value', component: GetValueComponent, title: 'Get Value' },
