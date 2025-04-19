b90797f47e0e0c68e6b6a7bc8153cb82e9f55dfb
lana rodion <lana.rodion@gmail.com>
2025-04-19 21:17:38 +0200

    Add UpdateLocation component

---
+++ src/app/components/update-location/update-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-location',
+  imports: [],
+  templateUrl: './update-location.component.html',
+  styleUrl: './update-location.component.css'
+})
+export class UpdateLocationComponent {
+
+}
