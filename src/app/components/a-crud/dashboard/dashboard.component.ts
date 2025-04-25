c634d106a1245c3c7e3fea6d156882c4ca7aab19
lana rodion <lana.rodion@gmail.com>
2025-04-25 21:39:52 +0200

    WIP Dashboard EmployeesList

--- src/app/components/a-crud/dashboard/dashboard.component.ts
+++ src/app/components/a-crud/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { GlobalService } from '../../../services/global.service';
+import { EmployeeService } from '../../../services/employee.service';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { Employee } from '../employee';
 
@@ -19,33 +19,32 @@ import { Employee } from '../employee';
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
-  empDataArray: Array<Employee> = []; // To store our employees data
+  empList: Employee[] = []; // To store our employees data
   // term: any; // for search bar
   p: number = 1; // for pagination
   countPerPage: number = 8; // number of rows per page
   count: number = 0;
 
-  constructor(private globalService: GlobalService) {}
+  constructor(public employeeService: EmployeeService) {}
 
   ngOnInit(): void {
     // Initialize the component when it is loaded
-    this.globalService.getRecords('employees').subscribe((res) => {
-      this.empDataArray = res as Employee[]; // Get all records on screen
-      this.empDataArray.forEach((emp: any) => {
-        emp.id = JSON.parse(emp.id); // Parse the ID
-      });
-      this.count = this.empDataArray.length;
+    this.getEmployeesList();
+  }
+
+  getEmployeesList() {
+    this.employeeService.getRecords().subscribe((res: Employee[]) => {
+      this.empList = res; // Get all records on screen
+      this.count = this.empList.length;
+      // console.log('this.empList: ', this.empList);
     });
   }
 
   delete(id: any) {
-    this.globalService.deleteRecord('employees', id).subscribe(() => {
-      this.empDataArray.splice(
-        this.empDataArray.findIndex((emp) => emp.id === id),
-        1
-      );
+    this.employeeService.deleteRecord(id).subscribe(() => {
+      this.empList.findIndex((emp) => emp.id === id);
       // Remove the deleted record from the array
-      this.count = this.empDataArray.length; // Update the count
+      this.count = this.empList.length; // Update the count
       alert('Record Deleted Successfully');
     });
   }
